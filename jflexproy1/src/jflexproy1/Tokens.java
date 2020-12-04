85c6e1d8b9e1f387512120e8b674c9337402c876
LuisFernando117 <72579893+LuisFernando117@users.noreply.github.com>
2020-12-04 11:20:04 -0600

    Add files via upload

---
+++ jflexproy1/src/jflexproy1/Tokens.java
@@ -0,0 +1,40 @@
+package jflexproy1;
+
+
+public enum Tokens {
+   Linea,
+    Comillas,
+    T_dato,
+    Cadena,
+    If,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_incremento,
+    Op_relacional,
+    Op_atribucion,
+    Op_booleano,
+    Parentesis_a,
+    Parentesis_c,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    P_coma,
+    
+    Dos_Puntos,
+    Coma_Simple,
+    
+    Identificador,
+    Numero,
+    ERROR
+}
+
